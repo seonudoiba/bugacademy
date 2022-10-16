abd4f9be1930c5148e1f7a75bfbc3fba52dc9c4b
seonudoiba <abiodunigbehinadun17@gmail.com>
2022-10-16 03:49:20 +0100

    Add PAyment

--- components/PayButton.js
+++ components/PayButton.js
@@ -2,10 +2,10 @@ import React from "react";
 import Link from "next/link";
 const PayButton = () => {
 	return (
-		<div>
+		<div className="mb-36 w-32 p-4 text-center mx-32 rounded-xl bg-green-800">
 			<Link
 				href="/Payment"
-				className="mb-36 w-32 p-4 text-center mx-32 rounded-xl bg-green-800">
+				>
 				Pay Now
 			</Link>
 		</div>
