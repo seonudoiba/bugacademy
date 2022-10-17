73f1cb76becea4e6d3e63fa5a16e512660af2545
seonudoiba <abiodunigbehinadun17@gmail.com>
2022-10-17 01:18:48 +0100

    All

--- pages/Courses.js
+++ pages/Courses.js
@@ -4,7 +4,7 @@ import PaymentIcon from "@mui/icons-material/Payment";
 
 const Courses = () => {
 	return (
-		<div>
+		<div className="w-full">
 			<div className="md:h-1/5 md:p-28 p-12 hero  text-center md:text-left">
 				<div className="flex flex-col items-center">
 					<div className="text-2xl md:text-4xl  font-bold text-white p-2">
